0824a55c5d68ad0f2509c90c49e42743e3cf135a
Michael McDonald <Michael McDonald@localhost>
2009-11-06 10:29:09 +0000

    Map all PowerPoint SlideShow shortcut keys. Add ability to black or white screen from shortcut key.

---
+++ App_Code/User32.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class User32
+{
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+    public struct Rect
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool ClipCursor(ref Rect lpRect);
+
+    [DllImport("user32.dll")]
+    public static extern bool GetClipCursor(ref Rect lpRect);
+}
